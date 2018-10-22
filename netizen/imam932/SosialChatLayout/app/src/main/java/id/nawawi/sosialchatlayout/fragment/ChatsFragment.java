a666f272baf4835ad355093b796bad5be06632e4
imam <nawawi932@gmail.com>
2018-10-22 08:10:58 +0700

    membuat fungsi tes

--- netizen/imam932/SosialChatLayout/app/src/main/java/id/nawawi/sosialchatlayout/fragment/ChatsFragment.java
+++ netizen/imam932/SosialChatLayout/app/src/main/java/id/nawawi/sosialchatlayout/fragment/ChatsFragment.java
@@ -26,5 +26,6 @@ public class ChatsFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_chats, container, false);
     }
+    public tes(){}
 
 }
